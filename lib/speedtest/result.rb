794cd98c0632df9c77cd52a491cbb79d0150b65b
myron <pete.myron@gmail.com>
2016-04-30 00:07:38 -0700

    added pretty representations of download/upload rates

--- lib/speedtest/result.rb
+++ lib/speedtest/result.rb
@@ -1,11 +1,13 @@
 module Speedtest
   class Result
-    attr_accessor :server, :latency, :download_rate, :upload_rate
+    attr_accessor :server, :latency, :download_rate, :pretty_download_rate, :upload_rate, :pretty_upload_rate
     def initialize(values = {})
       @server = values[:server] rescue nil
       @latency = values[:latency] rescue nil
       @download_rate = values[:download_rate] rescue nil
       @upload_rate = values[:upload_rate] rescue nil
+      @pretty_download_rate = values[:pretty_download_rate] rescue nil
+      @pretty_upload_rate = values[:pretty_upload_rate] rescue nil
     end
   end
 end
